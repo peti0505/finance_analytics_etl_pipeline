a6e244fb6d58e2f70a4bc4a071d36b3612e53eea
Hauzer Péter <phauzer05@gmail.com>
2026-04-17 20:01:40 +0200

    feat: add date dimension table for Power Bi star schema use

---
+++ date_dimension.sql
@@ -0,0 +1,38 @@
+USE finance_project
+SET NOCOUNT ON
+GO
+
+DECLARE @datefiller DATE
+
+IF OBJECT_ID('dbo.date_dim') IS NOT NULL
+    DROP TABLE date_dim
+
+CREATE TABLE date_dim
+(
+    dates_dim DATE PRIMARY KEY, 
+    year_dim INT, 
+    month_dim INT,
+    monthname_dim VARCHAR(9), 
+    day_dim INT,
+    weekday_dim INT,
+    weekdayname_dim VARCHAR(9)
+)
+
+SET @datefiller = '2000-01-01'
+
+WHILE @datefiller != '2100-01-01'
+BEGIN
+    INSERT INTO date_dim 
+    VALUES
+    (@datefiller, 
+    YEAR(@datefiller), 
+    MONTH(@datefiller), 
+    DATENAME(M, @datefiller),
+    DAY(@datefiller),
+    CASE WHEN (DATEPART(dw, @datefiller)+6) % 7 = 0 THEN 7
+        ELSE ((DATEPART(dw, @datefiller)) + 6) % 7
+    END,
+    DATENAME(DW, @datefiller) 
+    )
+    SET @datefiller = DATEADD(DAY, 1, @datefiller)
+END
