34b50c21717da55a36ab79b2cbb6fd827c64875b
Hauzer Péter <phauzer05@gmail.com>
2026-04-18 11:58:27 +0200

    feat: add date dimension view for Power Bi use

--- data_modeling.sql
+++ data_modeling.sql
@@ -89,3 +89,16 @@ GO
 
 
 
+CREATE OR ALTER VIEW [dbo].[v_date_dimension] AS
+SELECT
+*
+FROM 
+    date_dim
+WHERE  
+    dates_dim >= (SELECT MIN(Date_only) FROM finance_table)
+    AND dates_dim <= (SELECT MAX(Date_only) FROM finance_table)
+GO
+
+
+
+
