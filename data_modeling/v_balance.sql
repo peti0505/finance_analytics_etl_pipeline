d49142012f412f40881113e0c40124a2ab242e8f
Hauzer Péter <phauzer05@gmail.com>
2026-04-19 22:20:25 +0200

    refactor: split SQL file into separate view models and organize modeling folder

---
+++ data_modeling/v_balance.sql
@@ -0,0 +1,34 @@
+CREATE OR ALTER   view [dbo].[v_balance] AS
+With byday AS
+(
+SELECT
+    Date_only AS Date 
+    ,Sum(Amount) AS daily_movement
+FROM
+    finance_table
+GROUP BY 
+    Date_only
+),
+
+accumulated AS
+(
+SELECT 
+    dates_dim
+    ,COALESCE(daily_movement,0) as daily_movement
+    ,SUM(COALESCE(daily_movement,0)) OVER(ORDER BY dates_dim) AS running_total
+FROM 
+    date_dim as dd  
+    LEFT JOIN byday as bd
+    ON dd.dates_dim = bd.date
+WHERE 
+    dd.dates_dim  >= (SELECT MIN(date) FROM byday)
+    AND dd.dates_dim <= (SELECT MAX(date) FROM byday)
+)
+
+SELECT
+    dates_dim AS Date
+    ,running_total
+    ,AVG(running_total) OVER(ORDER BY dates_dim ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) AS moving_average
+FROM
+    accumulated
+GO
---
+++ data_modeling/v_transaction_master.sql
@@ -0,0 +1,12 @@
+CREATE OR ALTER     VIEW [dbo].[v_transaction_master] AS
+SELECT
+    Date_only AS Date
+    ,DATENAME(dw, Date_only) AS Weekday
+    --Converting week start to monday to match european standards
+    ,CASE WHEN (DATEPART(dw, Date_only)+6) % 7 = 0 THEN 7
+        ELSE ((DATEPART(dw, Date_only)) + 6) % 7
+    END AS weekday_number
+    ,Amount
+FROM 
+    finance_table
+GO
