6a8fa592d5aff9b2a6afa72d053fa6530a161887
phauzer <phauzer05@gmail.com>
2026-05-25 16:50:56 +0200

    fix: remove v_yearly_Pareto_distribution.sql and calculate pareto in Power BI so it is more filterable

--- data_modeling/v_transaction_master.sql
+++ data_modeling/v_transaction_master.sql
@@ -1,6 +1,7 @@
 CREATE OR ALTER     VIEW [dbo].[v_transaction_master] AS
 SELECT
     Date_only AS Date
+    ,Partner_name
     ,DATENAME(dw, Date_only) AS Weekday
     --Converting week start to monday to match european standards
     ,CASE WHEN (DATEPART(dw, Date_only)+6) % 7 = 0 THEN 7
